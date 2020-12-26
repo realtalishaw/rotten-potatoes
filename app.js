80e9b59dc3ab633b3f91153b7281e1214a4857aa
Talisha <misstalishawhite@gmail.com>
2020-12-26 18:43:13 -0500

    users can see all reviews

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+
 // Middleware
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
+    res.render('reviews-index', { reviews: reviews });
 })
+
 
 // Server Port
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
+
+// Mock Array of Projects
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
