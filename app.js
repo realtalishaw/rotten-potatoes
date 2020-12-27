a02829f1989c1f9c73d11037e7b645bf718e2360
Talisha <misstalishawhite@gmail.com>
2020-12-26 19:29:12 -0500

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Require libraries
 const express = require('express')
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 // App setup
 const app = express()
@@ -8,12 +10,22 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String,
+});
 
 // Middleware
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 
@@ -23,8 +35,10 @@ app.listen(3000, () => {
 })
 
 // Mock Array of Projects
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II" },
-    { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+//let reviews = [
+  //  { title: "Great Review", movieTitle: "Batman II" },
+    //{ title: "Awesome Movie", movieTitle: "Titanic" },
+   // { title: "Two Thumbs Way, Way, Up!", movieTitle: "Hercules" },
+   // { title: "5 out of 5 highlt recommend", movieTitle: "Little Shop of Horrors"}
+//]
 
