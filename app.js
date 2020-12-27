193391174ed7e0ba3d92b084404a738945286c98
Talisha <misstalishawhite@gmail.com>
2020-12-27 14:04:40 -0500

    deleteing comments

--- app.js
+++ app.js
@@ -126,3 +126,13 @@ app.post('/reviews/comments', (req, res) => {
       });
     });
 
+// DELETE
+app.delete('/reviews/comments/:id', function (req, res) {
+  console.log("DELETE comment")
+  Comment.findByIdAndRemove(req.params.id).then((comment) => {
+    res.redirect(`/reviews/${comment.reviewId}`);
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+	  
