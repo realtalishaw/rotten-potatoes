45d4d348dbbda1c919370c58d87ce13fa2e0fe27
Talisha <misstalishawhite@gmail.com>
2020-12-26 22:02:03 -0500

    Users can see single reviews

--- app.js
+++ app.js
@@ -34,8 +34,7 @@ const Review = mongoose.model('Review', {
 app.get('/', (req, res) => {
     Review.find()
 	.then(reviews => {
-	    console.log(reviews)
-     res.render('reviews-index', { Review: reviews });
+	res.render('reviews-index', { reviews: reviews });
    }).catch(err => {
       console.log(err);
    })
@@ -56,7 +55,7 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
       console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`);
   }).catch((err) => {
     console.log(err.message);
   })
