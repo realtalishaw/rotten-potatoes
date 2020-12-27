8c2b9a6804ac1364be8c00e970232ee2c955b201
Talisha <misstalishawhite@gmail.com>
2020-12-26 22:59:56 -0500

    Users can delete reviews

--- app.js
+++ app.js
@@ -97,3 +97,13 @@ app.put('/reviews/:id', (req, res) => {
 	    console.log(err.message)
 	})
 })
+
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+    console.log("DELETE review")
+    Review.findByIdAndRemove(req.params.id).then((review) => {
+	res.redirect('/');
+    }).catch((err) => {
+	console.log(err.message);
+    })
+})
