8f10f25c61b9838f025ceda6dae212e289271d2a
Talisha <misstalishawhite@gmail.com>
2020-12-28 10:17:58 -0500

    finally made cookies

--- app.js
+++ app.js
@@ -166,11 +166,12 @@ app.post("/sign-up", (req, res) => {
   user
     .save()
     .then(user => {
-      var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+	var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+	res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
       res.redirect("/");
     })
     .catch(err => {
-      console.log(err.message);
+      console.log(err.message + process.env.SECRET);
       return res.status(400).send({ err: err });
     });
 });
