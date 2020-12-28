1b0d851c26164122458c2a73b8219a1276ed9547
Talisha <misstalishawhite@gmail.com>
2020-12-28 10:33:28 -0500

    user system is working now

--- app.js
+++ app.js
@@ -47,6 +47,43 @@ const Review = mongoose.model('Review', {
 // Middleware
 app.use(methodOverride('_method'))
 
+// LOGIN                                                                               
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Find this user name                                                               
+  User.findOne({ username }, "username password")
+    .then(user => {
+      if (!user) {
+        // User not found                                                              
+        return res.status(401).send({ message: "Wrong Username or Password" });
+      }
+      // Check the password                                                            
+      user.comparePassword(password, (err, isMatch) => {
+        if (!isMatch) {
+          // Password does not match                                                   
+          return res.status(401).send({ message: "Wrong Username or password" });
+        }
+        // Create a token                                                              
+        const token = jwt.sign({ _id: user._id, username: user.username }, process.env
+.SECRET, {
+          expiresIn: "60 days"
+        });
+          // Set a cookie and redirect to root                                         
+          console.log("user is: " + username);
+        res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+        res.redirect("/");
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 // Check Login status
 var checkAuth = (req, res, next) => {
   console.log("Checking authentication");
@@ -168,7 +205,7 @@ app.post("/sign-up", (req, res) => {
     .then(user => {
 	var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
 	res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-      res.redirect("/");
+	res.redirect("/");
     })
     .catch(err => {
       console.log(err.message + process.env.SECRET);
@@ -178,42 +215,9 @@ app.post("/sign-up", (req, res) => {
 
 // LOG OUT
 app.get('/logout', (req, res) => {
-  res.clearCookie('nToken');
+    res.clearCookie('nToken');
+    console.log("logged out");
   res.redirect('/');
 });
 
-// LOGIN 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  // Find this user name
-  User.findOne({ username }, "username password")
-    .then(user => {
-      if (!user) {
-        // User not found
-        return res.status(401).send({ message: "Wrong Username or Password" });
-      }
-      // Check the password
-      user.comparePassword(password, (err, isMatch) => {
-        if (!isMatch) {
-          // Password does not match
-          return res.status(401).send({ message: "Wrong Username or password" });
-        }
-        // Create a token
-        const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
-          expiresIn: "60 days"
-        });
-        // Set a cookie and redirect to root
-        res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
-        res.redirect("/");
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
 
